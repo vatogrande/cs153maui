05be4789a2bca5f767e47f0658cccfc0e2518d7d
saeteurn81@gmail.com <saeteurn81@gmail.com@f477e21d-d002-f388-c5f0-ae7f19323745>
2011-02-09 01:31:08 +0000

    Java number token written and tested under PascalNumberToken.java

--- JavaParser/src/wci/frontend/pascal/tokens/PascalNumberToken.java
+++ JavaParser/src/wci/frontend/pascal/tokens/PascalNumberToken.java
@@ -57,21 +57,49 @@ public class PascalNumberToken extends PascalToken
         char currentChar;              // current character
 
         type = INTEGER;  // assume INTEGER token type for now
+        currentChar = currentChar();
+        
+        //Check for zero '0' at beginning
+        
+        if(currentChar == '0'){
+        	if (peekChar() == 'x'){
+        		type = HEX;
+        		String hexDigits = extractHexNumber(textBuffer);
+        		if (type == ERROR){
+        			return;
+        		}
+        		
+        		int integerValue = getHexValue(hexDigits);
 
-        // Extract the digits of the whole part of the number.
-        wholeDigits = unsignedIntegerDigits(textBuffer);
-        if (type == ERROR) {
-            return;
+                if (type != ERROR) {
+                    value = new Integer(integerValue);
+                }
+        		
+        		return;
+        	}
+        	else{
+        		type = OCT;
+        		String octDigits = extractOctNumber(textBuffer);
+        		if(type == ERROR){return;}
+        		
+        		int intergerValue = getOctValue(octDigits);
+        		 if (type != ERROR){
+        			 value = new Integer(intergerValue);
+        		 }
+        		
+        		return;
+        	}
+        	
+        	
         }
+        
+        
+
 
         // Is there a . ?
         // It could be a decimal point or the start of a .. token.
-        currentChar = currentChar();
+        
         if (currentChar == '.') {
-            if (peekChar() == '.') {
-                sawDotDot = true;  // it's a ".." token, so don't consume it
-            }
-            else {
                 type = REAL;  // decimal point, so token type is REAL
                 textBuffer.append(currentChar);
                 currentChar = nextChar();  // consume decimal point
@@ -81,6 +109,12 @@ public class PascalNumberToken extends PascalToken
                 if (type == ERROR) {
                     return;
                 }
+            
+        }else{
+            // Extract the digits of the whole part of the number.
+            wholeDigits = unsignedIntegerDigits(textBuffer);
+            if (type == ERROR) {
+                return;
             }
         }
 
@@ -116,13 +150,218 @@ public class PascalNumberToken extends PascalToken
         else if (type == REAL) {
             float floatValue = computeFloatValue(wholeDigits, fractionDigits,
                                                  exponentDigits, exponentSign);
-
             if (type != ERROR) {
                 value = new Float(floatValue);
             }
         }
     }
 
+    
+    //Extract and return the digits of a hex number
+    private String extractHexNumber (StringBuilder textBuffer)
+    	throws Exception{
+    	char currentChar = currentChar();
+    	
+        // Must have at least one digit.
+        if (!Character.isDigit(currentChar) || peekChar() != 'x') {
+            type = ERROR;
+            value = INVALID_NUMBER;
+            return null;
+        }
+        else{
+        	type = HEX;
+            StringBuilder digits = new StringBuilder();
+        	//take the first two char (should be '0x')
+            textBuffer.append(currentChar);
+        	digits.append(currentChar);
+        	currentChar = nextChar();
+        	textBuffer.append(currentChar);
+        	digits.append(currentChar);
+            // Extract hex digits.
+
+            boolean hexDigit = true;
+            while (hexDigit) {
+            	
+                currentChar = nextChar();  // consume digit
+                hexDigit = false;
+                switch (currentChar){
+                case 'a':
+                case 'A':
+                case 'b':
+                case 'B':                
+                case 'c':                
+                case 'C':                
+                case 'd':
+                case 'D':
+                case 'e':
+                case 'E':
+                case 'f':
+                case 'F':
+                	hexDigit = true;
+                	break;
+                default:
+                	
+                }
+                
+                if((hexDigit == true) || Character.isDigit(currentChar)){
+                	textBuffer.append(currentChar);
+                	digits.append(currentChar);
+                	hexDigit = true;
+                }             	
+            }
+
+            return digits.toString();
+        	
+        }	
+    }
+    
+    /**
+     * returns the decimal value of a string of hex digits
+     * @param hexDigits string of hex digits
+     * @return int value of hex string
+     */
+    private int getHexValue(String hexDigits){
+    	if (hexDigits == null){
+    		return 0;
+    	}
+    	 
+    	//remove hex id "0x"
+    	hexDigits = hexDigits.replaceFirst("0x","");
+    	
+    	int currentHexCharValue = 0;
+    	int integerValue = 0;
+         int prevValue = -1;    // overflow occurred if prevValue > integerValue
+         int index = 0;
+
+         // Loop over the digits to compute the integer value
+         // as long as there is no overflow.
+         hexDigits.toLowerCase();
+         char currentHexChar =' ';
+         while ((index < hexDigits.length()) && (integerValue >= prevValue)) {
+             prevValue = integerValue;
+             currentHexChar = hexDigits.charAt(index);
+             switch (currentHexChar){
+             case 'a':
+             case 'A': 
+            	 currentHexCharValue = 10;
+            	 break;
+             case 'b':
+             case 'B': 
+            	 currentHexCharValue = 11;
+            	 break;
+             case 'c':                
+             case 'C': 
+            	 currentHexCharValue = 12;
+            	 break;
+             case 'd':
+             case 'D':
+            	 currentHexCharValue = 13;
+            	 break;
+             case 'e':
+             case 'E':
+            	 currentHexCharValue = 14;
+            	 break;
+             case 'f':
+             case 'F':
+            	 currentHexCharValue = 15;
+            	 break;
+             default:
+             
+            	 currentHexCharValue = Character.getNumericValue(currentHexChar);
+             }
+             integerValue = 16*integerValue + currentHexCharValue;
+             index++;
+         }
+
+         // No overflow:  Return the integer value.
+         if (integerValue >= prevValue) {
+        	 //System.out.println("hex : " + hexDigits+" hex value :" + integerValue);
+             return integerValue;
+         }
+
+         // Overflow:  Set the integer out of range error.
+         else {
+        	 //System.out.println("hex had an error");
+             type = ERROR;
+             value = RANGE_INTEGER;
+             return 0;
+         }
+
+    }
+    
+    /**
+     * Extracts character for a number in oct
+     * @param textBuffer buffer for numbers to be stored
+     * @return retuns the number in string formate
+     * @throws Exception
+     */
+    private String extractOctNumber(StringBuilder textBuffer)
+    throws Exception{
+    	
+    	char currentChar = currentChar();
+    	
+        // Must have at least one digit.
+        if (!Character.isDigit(currentChar) || currentChar != '0') {
+            type = ERROR;
+            value = INVALID_NUMBER;
+            return null;
+        }
+        else{
+        	type = OCT;
+            StringBuilder digits = new StringBuilder();
+
+            // Extract oct digits.
+            boolean octDigit = true;
+            while (octDigit) {
+            	if(currentChar == '9' || currentChar == '8' || !Character.isDigit(currentChar)){
+            		octDigit = false;
+            	}else{
+            		System.out.print(currentChar);
+	                if((octDigit == true) || Character.isDigit(currentChar)){
+	                	textBuffer.append(currentChar);
+	                	digits.append(currentChar);
+	                	currentChar = nextChar();
+	                }
+            	}
+            }
+            return digits.toString();
+        }
+    }
+    
+    private int getOctValue(String octDigits){
+    	if (octDigits == null){
+    		return 0;
+    	}
+    	     	
+    	int integerValue = 0;
+         int prevValue = -1;    // overflow occurred if prevValue > integerValue
+         int index = 0;
+
+         // Loop over the digits to compute the integer value
+         // as long as there is no overflow.
+         while ((index < octDigits.length()) && (integerValue >= prevValue)) {
+             prevValue = integerValue;
+             integerValue = 8*integerValue +
+                            Character.getNumericValue(octDigits.charAt(index++));
+         }
+
+         // No overflow:  Return the integer value.
+         if (integerValue >= prevValue) {
+        	 //System.out.println("hex : " + hexDigits+" hex value :" + integerValue);
+             return integerValue;
+         }
+
+         // Overflow:  Set the integer out of range error.
+         else {
+        	 //System.out.println("hex had an error");
+             type = ERROR;
+             value = RANGE_INTEGER;
+             return 0;
+         }
+
+    	
+    }
+    
     /**
      * Extract and return the digits of an unsigned integer.
      * @param textBuffer the buffer to append the token's characters.
